c8708489909f831844c9124dc7b1ca010805b21d
commanderkid <nafiguspofigus@gmail.com>
2017-05-25 06:43:47 +0300

    Compare two algoritms
    Which of them works faster

--- 1.1 o(n^2).py
+++ 1.1 o(n^2).py
@@ -1,16 +1,23 @@
 ##O(f(N)*g(N)) algorithm realisation
-a=[9, 8, 7, 3, 5, 7, 1, 2, 3, 4, 5, 6]
-b=[11, 22, 44, 55, 44, 33, 54, 26, 24, 52, 55, 6]
+a = [9, 8, 7, 3, 5, 7, 1, 2, 3, 4, 5, 6]
+b = [11, 22, 44, 55, 44, 33, 4, 26, 24, 52, 55, 6]
 i, j = 0, 0
 for i in range(len(a)-1):
     for j in range(len(b)-1):
-        if (i!=j):
-            print(a, b)
-            if a[i]==a[j]:
+        if (i != j):
+            if a[i] == b[j]:
                 print("True", i, j)
-                print(a, b)
+                print(a[i], b[j])
             else:
-                j=j+1
+                j = j+1
         else:
-            i=i+1
+            i = i+1
+###--------------------------------------------------###
+i, j = 0, 0
+for i in range(len(a)-2):
+    for j = i+1 in range(len(a)-2):
+        if a[i] == a[j]:
+            print("True")
+        else:
             
+    
